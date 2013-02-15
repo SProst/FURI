2767d5309857c88252f8a5e985818c8aedc3421b
Spencer Prost <spencer.prost@live.com>
2013-02-15 09:58:07 -0700

    Updated Spatial

--- Spatial.cpp
+++ Spatial.cpp
@@ -1,52 +1,73 @@
-//#include "Spatial.h"
-//#include <iostream>
-//
-//using namespace std;
-//
-//Spatial::Spatial(void)
-//{
-//	this->spatial = 0;
-//
-//
-////   CPhidgetSpatial_set_OnSpatialData_Handler(this->spatial, SpatialDataHandler, NULL);
-//}
-//
-//void Spatial::registerHandlers(void)
-//{
-//CPhidget_set_OnAttach_Handler((CPhidgetHandle)this->spatial, &Spatial::AttachHandler, this);
-//  CPhidget_set_OnDetach_Handler((CPhidgetHandle)this->spatial, &Spatial::DetachHandler, this);
-// // CPhidget_set_OnError_Handler((CPhidgetHandle)this->spatial, &Spatial::ErrorHandler, this);
-//}
-//
-//int Spatial::getDeviceSerialNumber()
-//{
-//	int serNum;
-//	CPhidget_getSerialNumber(spatial, &serNum);
-//	return serNum;
-//}
-//
-//void Spatial::attachHandler()
-//{
-//	cout << "Phidget attached : " << getDeviceSerialNumber() << endl;
-//}
-//
-//int CCONV Spatial::AttachHandler(CPhidgetHandle spatial, void *userptr)
-//{
-//	((Spatial*)userptr)->attachHandler();
-//
-//	return 0;
-//}
-//
-//int CCONV Spatial::DetachHandler(CPhidgetHandle spatial, void *userptr)
-//{
-//	int serialNo;
-//	CPhidget_getSerialNumber(spatial, &serialNo);
-//	cout << "Spatial" << serialNo << "detached!" << endl;
-//	return 0;
-//}
-//
-//
-//Spatial::~Spatial(void)
-//{
-//	CPhidget_delete((CPhidgetHandle)this->spatial);
-//}
+#include "Spatial.h"
+#include <iostream>
+
+using namespace std;
+
+int CCONV AttachHandler(CPhidgetHandle spatial, void *userptr)
+{
+	((Spatial*)userptr)->attachHandler();
+
+	return 0;
+}
+
+int CCONV DetachHandler(CPhidgetHandle spatial, void *userptr)
+{
+	((Spatial*)userptr)->detachHandler();
+	return 0;
+}
+
+int CCONV ErrorHandler(CPhidgetHandle handle, void *userptr, int ErrorCode, const char *unknown)
+{
+	((Spatial*)userptr)->errorHandler(ErrorCode);
+	return 0;
+}
+Spatial::Spatial(void)
+{
+	this->spatial = 0;
+
+
+//   CPhidgetSpatial_set_OnSpatialData_Handler(this->spatial, SpatialDataHandler, NULL);
+}
+
+void Spatial::registerHandlers(void)
+{
+CPhidget_set_OnAttach_Handler((CPhidgetHandle)this->spatial, AttachHandler, this);
+  CPhidget_set_OnDetach_Handler((CPhidgetHandle)this->spatial, DetachHandler, this);
+  CPhidget_set_OnError_Handler((CPhidgetHandle)this->spatial, ErrorHandler, this);
+}
+
+std::string Spatial::getErrorDescription(int errorCode)
+{
+	char a[1000];
+  const char * errorPtr = a;
+  CPhidget_getErrorDescription(errorCode, &errorPtr);
+  return std::string(errorPtr);
+}
+
+int Spatial::getDeviceSerialNumber()
+{
+	int serNum;
+	CPhidget_getSerialNumber(spatial, &serNum);
+	return serNum;
+}
+
+void Spatial::attachHandler()
+{
+	cout << "Phidget attached : " << getDeviceSerialNumber() << endl;
+}
+
+void Spatial::detachHandler()
+{
+	cout << "Phidget detached : " << getDeviceSerialNumber() << endl;
+}
+
+void Spatial::errorHandler(int error)
+{
+	cout << "Phidget error " << error << getErrorDescription(error).c_str();
+}
+
+
+Spatial::~Spatial(void)
+{
+	CPhidget_delete((CPhidgetHandle)this->spatial);
+}
--- Spatial.h
+++ Spatial.h
@@ -1,32 +1,34 @@
-//#pragma once
-//#include "phidget21.h"
-//class Spatial
-//{
-//public:
-//	Spatial(void);
-//	CPhidgetSpatial_SpatialEventDataHandle data;
-//	double Gx,Gy,Gz;
-//	void getAcc(int axis);
-//	void open(void);
-//	int wait(int milliseconds);
-//	 int displayProperties(void);
-//    static int SpatialDataHandler(CPhidgetSpatialHandle spatial, void *userptr, CPhidgetSpatial_SpatialEventDataHandle *data, int count);
-//    void setDataRate(int ms);
-//    void getAcc(void);
-//    void close(void);
-//	virtual ~Spatial(void);
-//	virtual void registerHandlers();
-//    virtual void attachHandler();
-//    virtual void detachHandler();
-//    virtual void errorHandler(int error);
-//private: 
-//	CPhidgetHandle spatial;
-//	int serialNo, version;
-//	int getDeviceSerialNumber(void);
-//	const char* DeviceType;
-//	int numAccelAxes, numGyroAxes, numCompassAxes, dataRateMax, dataRateMin;
-//	int CCONV AttachHandler(CPhidgetHandle spatial, void *userptr);
-//    int CCONV DetachHandler(CPhidgetHandle spatial, void *userptr);
-//    int CCONV ErrorHandler(CPhidgetHandle spatial, void *userptr, int ErrorCode, const char *unknown);
-//};
+#pragma once
+#include "phidget21.h"
+#include <string>
+class Spatial
+{
+public:
+	Spatial(void);
+	CPhidgetSpatial_SpatialEventDataHandle data;
+	double Gx,Gy,Gz;
+	void getAcc(int axis);
+	void open(void);
+	int wait(int milliseconds);
+	 int displayProperties(void);
+    static int SpatialDataHandler(CPhidgetSpatialHandle spatial, void *userptr, CPhidgetSpatial_SpatialEventDataHandle *data, int count);
+    void setDataRate(int ms);
+    void getAcc(void);
+    void close(void);
+	virtual ~Spatial(void);
+	virtual void registerHandlers();
+    virtual void attachHandler();
+    virtual void detachHandler();
+    virtual void errorHandler(int error);
+	std::string getErrorDescription(int errorCode);
+private: 
+	CPhidgetHandle spatial;
+	int serialNo, version;
+	int getDeviceSerialNumber(void);
+	const char* DeviceType;
+	int numAccelAxes, numGyroAxes, numCompassAxes, dataRateMax, dataRateMin;
+	//int CCONV AttachHandler(CPhidgetHandle spatial, void *userptr);
+ //   int CCONV DetachHandler(CPhidgetHandle spatial, void *userptr);
+ //   int CCONV ErrorHandler(CPhidgetHandle spatial, void *userptr, int ErrorCode, const char *unknown);
+};
 
