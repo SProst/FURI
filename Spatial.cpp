88d1eb941007a56c8f13b02e34b63ba8a0bd36f1
Spencer Prost <spencer.prost@live.com>
2013-02-15 12:21:30 -0700

    Updated Spatial

--- Spatial.cpp
+++ Spatial.cpp
@@ -7,12 +7,11 @@ using namespace std;
 namespace phidgets {
 Spatial::Spatial(void)
 {
-	this->spatial = 0;
+	Phidget();
 	
 }
 
 Spatial::~Spatial(void)
 {
-	CPhidget_delete((CPhidgetHandle)this->spatial);
 }
 }
--- Spatial.h
+++ Spatial.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "phidget21.h"
+#include "Phidget.h"
 #include <string>
 
 namespace phidgets {
@@ -7,24 +8,22 @@ class Spatial
 {
 public:
 	Spatial(void);
-	CPhidgetSpatial_SpatialEventDataHandle data;
 	double Gx,Gy,Gz;
 	void getAcc(int axis);
-	void open(void);
 	int wait(int milliseconds);
 	 int displayProperties(void);
-    static int SpatialDataHandler(CPhidgetSpatialHandle spatial, void *userptr, CPhidgetSpatial_SpatialEventDataHandle *data, int count);
     void setDataRate(int ms);
     void getAcc(void);
-    void close(void);
 	virtual ~Spatial(void);
-	std::string getErrorDescription(int errorCode);
+protected: 
+	CPhidgetSpatialHandle spatial_handle_;
+
+	void zero();
+	void setDataRate(int rate);
+
+	virtual void dataHandler(CPhidgetSpatial_SpatialEventDataHandle *data, int count);
 private: 
-	CPhidgetHandle spatial;
-	int serialNo, version;
-	int getDeviceSerialNumber(void);
-	const char* DeviceType;
-	int numAccelAxes, numGyroAxes, numCompassAxes, dataRateMax, dataRateMin;
+	static int CCONV SpatialDataHandler(CPhidgetSpatialHandle spatial, void *userptr, CPhidgetSpatial_SpatialEventDataHandle *data, int count);
 };
 
 }
